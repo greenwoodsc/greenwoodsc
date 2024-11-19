e359cb6a0d9f67e21f3b13d0300769a8e6eb9632
windhamdavid <david@davidawindham.com>
2024-11-19 15:10:02 -0500

    crrect edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/windhamdavid/thewood/src/main/',
+            'https://github.com/windhamdavid/thewood/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
